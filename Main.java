63117a149e0fd62b04234afa5ae1c694be41db69
Miguel Angel <mamiglesias2006@gmail.com>
2025-11-21 12:02:27 +0100

    while añadido

---
+++ Main.java
@@ -0,0 +1,20 @@
+import javax.xml.transform.Source;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("1 = ");
+        }
+        if (true) {
+            System.out.println("Hello desde develop");
+        }
+        int j = 0;
+        while (true) {
+           System.out.println("j = " + j);
+        }
+    }
+}
